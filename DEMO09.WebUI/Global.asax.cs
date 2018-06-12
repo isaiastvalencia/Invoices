abdeb0097ae40559e4154a7354470243e70425f9
Isaias Torres <isaiastvalencia@hotmail.com>
2018-06-12 15:43:37 -0500

    pruebas

--- DEMO09.WebUI/Global.asax.cs
+++ DEMO09.WebUI/Global.asax.cs
@@ -15,6 +15,7 @@ namespace DEMO09.WebUI
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
+
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
